70b219371f196c97e4f814f78456c782dd1e2b0c
JeremyPerezSwift <jeremyperez34000@gmail.com>
2021-12-29 13:53:40 +0100

    Initial Commit

---
+++ YourMovieList/YourMovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourMovieListApp.swift
+//  YourMovieList
+//
+//  Created by Jérémy Perez on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct YourMovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
